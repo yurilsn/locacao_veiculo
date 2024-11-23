2fe4f4efbb2608d6457de86619e0d2406de600b0
yurilsn <luizyuri57@gmail.com>
2024-11-23 17:58:55 -0300

    feat: implementando função locar

--- src/main/java/com/modelacao/locacao_veiculo/service/LocacaoService.java
+++ src/main/java/com/modelacao/locacao_veiculo/service/LocacaoService.java
@@ -1,7 +1,18 @@
 package com.modelacao.locacao_veiculo.service;
 
+import com.modelacao.locacao_veiculo.domain.Locacao;
+import com.modelacao.locacao_veiculo.domain.Usuario;
+import com.modelacao.locacao_veiculo.repository.LocacaoRepository;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class LocacaoService {
 
-    public void locar(){}
+    private LocacaoRepository locacaoRepository;
+
+    public void locar(Usuario usuario, Locacao locacao){
+        locacao.setUsuarioId(usuario);
+        locacaoRepository.save(locacao);
+    }
     public void finalizarLocacao(){}
 }
