b57b7f779508379edcf39ce1cd34ae3c04b2d075
yurilsn <luizyuri57@gmail.com>
2024-11-23 18:01:50 -0300

    feat: Implementando busca por locacaoId em VeiculoRepository

--- src/main/java/com/modelacao/locacao_veiculo/repository/VeiculoRepository.java
+++ src/main/java/com/modelacao/locacao_veiculo/repository/VeiculoRepository.java
@@ -1,4 +1,13 @@
 package com.modelacao.locacao_veiculo.repository;
 
-public interface VeiculoRepository {
+import com.modelacao.locacao_veiculo.domain.Locacao;
+import com.modelacao.locacao_veiculo.domain.Veiculo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
+
+    List<Veiculo> findByLocacaoId(Locacao locacao);
+
 }
