4baca52db6018fa62c4169e62ec3e0fa47b7b5a3
yurilsn <luizyuri57@gmail.com>
2024-11-23 01:48:43 -0300

    feat: implementando função de busca por email e senha

--- src/main/java/com/modelacao/locacao_veiculo/repository/UsuarioRepository.java
+++ src/main/java/com/modelacao/locacao_veiculo/repository/UsuarioRepository.java
@@ -4,5 +4,8 @@ import com.modelacao.locacao_veiculo.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmailAndSenha(String email, String senha);
 }
