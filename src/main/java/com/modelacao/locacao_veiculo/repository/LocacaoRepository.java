741d8e8452f02d917c188e1a0b64481dd126d8d9
yurilsn <luizyuri57@gmail.com>
2024-11-23 17:58:36 -0300

    refactor: comentando funcao de LocacaoRepository

--- src/main/java/com/modelacao/locacao_veiculo/repository/LocacaoRepository.java
+++ src/main/java/com/modelacao/locacao_veiculo/repository/LocacaoRepository.java
@@ -1,10 +1,16 @@
 package com.modelacao.locacao_veiculo.repository;
 
 import com.modelacao.locacao_veiculo.domain.Locacao;
+import com.modelacao.locacao_veiculo.domain.Usuario;
+import com.modelacao.locacao_veiculo.domain.Veiculo;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface LocacaoRepository extends JpaRepository<Locacao, Long> {
-    List<Locacao> findByUsuarioId(Long id);
+
+//    List<Locacao> findByUsuarioId(Usuario usuario);
+
 }
