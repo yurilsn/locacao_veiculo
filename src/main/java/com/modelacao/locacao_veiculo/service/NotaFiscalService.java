6bfd3ebf45f9576009920965695e4e675956de12
yurilsn <luizyuri57@gmail.com>
2024-11-23 17:59:49 -0300

    feat: implementando função de getValorTotal em NotaFiscalService

--- src/main/java/com/modelacao/locacao_veiculo/service/NotaFiscalService.java
+++ src/main/java/com/modelacao/locacao_veiculo/service/NotaFiscalService.java
@@ -1,6 +1,31 @@
 package com.modelacao.locacao_veiculo.service;
 
+import com.modelacao.locacao_veiculo.domain.Veiculo;
+import com.modelacao.locacao_veiculo.repository.LocacaoRepository;
+import com.modelacao.locacao_veiculo.repository.VeiculoRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class NotaFiscalService {
 
-    public void valorTotal(){}
+    private VeiculoRepository veiculoRepository;
+    private LocacaoRepository locacaoRepository;
+
+
+    public Integer getValorTotal(Long id){
+        Integer valor = 0;
+        List<Veiculo> veiculos =  veiculoRepository.findByLocacaoId(locacaoRepository.findById(id).orElseThrow());
+        for (Veiculo veiculo : veiculos){
+            valor += veiculo.getValor();
+        }
+        return valor;
+    }
+
+//    public Integer getValorTotal(Long id){
+//
+//    }
 }
