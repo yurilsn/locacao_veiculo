b5ab456a0056afc0b237be3db7cd69ff20180b9b
yurilsn <luizyuri57@gmail.com>
2024-11-23 17:59:28 -0300

    feat: implementando endpoint de valorTotal em NotaFiscalRest

--- src/main/java/com/modelacao/locacao_veiculo/rest/NotaFiscalRest.java
+++ src/main/java/com/modelacao/locacao_veiculo/rest/NotaFiscalRest.java
@@ -2,15 +2,17 @@ package com.modelacao.locacao_veiculo.rest;
 
 
 import com.modelacao.locacao_veiculo.domain.NotaFiscal;
-import com.modelacao.locacao_veiculo.domain.Usuario;
 import com.modelacao.locacao_veiculo.repository.LocacaoRepository;
+import com.modelacao.locacao_veiculo.repository.NotaFiscalRepository;
+import com.modelacao.locacao_veiculo.repository.UsuarioRepository;
 import com.modelacao.locacao_veiculo.service.NotaFiscalService;
+import com.modelacao.locacao_veiculo.service.UsuarioService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/api/nota_fiscal")
@@ -18,11 +20,26 @@ import java.util.Optional;
 public class NotaFiscalRest {
 
     private NotaFiscalService notaFiscalService;
+    private UsuarioRepository usuarioRepository;
+    private LocacaoRepository locacaoRepository;
+    private NotaFiscalRepository notaFiscalRepository;
 
-    @GetMapping("/valorTotal/{id}")
+    @GetMapping("/valorTotal/{idLocacao}/{idUsuario}")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<String> setValorTotal(@RequestParam("id") Long id) {
-        notaFiscalService.setValorTotal(id);
+    public ResponseEntity<String> ValorTotal(@RequestParam("idLocacao") Long idLocacao, @RequestParam("idUsuario") Long idUsuario) {
+        NotaFiscal notaFiscal = new NotaFiscal();
+        notaFiscal.setValorTotal(notaFiscalService.getValorTotal(idLocacao));
+        notaFiscal.setData(LocalDate.now());
+        notaFiscal.setLocal(usuarioRepository.findById(idUsuario).orElseThrow().getEmail());
+        notaFiscal.setIdLocacao(locacaoRepository.findById(idLocacao).orElseThrow());
+        notaFiscal.setIdUsuario(usuarioRepository.findById(idUsuario).orElseThrow());
+        notaFiscalRepository.save(notaFiscal);
+
         return ResponseEntity.ok().build();
     }
+
+//    @PostMapping
+//    public ResponseEntity<String> criarNota(){
+//
+//    }
 }
