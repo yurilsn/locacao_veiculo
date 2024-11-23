fd65d36a6d365884c3ab8737a55a344381685bbf
yurilsn <luizyuri57@gmail.com>
2024-11-23 01:49:18 -0300

    feat: implementando serviços de Usuario

--- src/main/java/com/modelacao/locacao_veiculo/service/UsuarioService.java
+++ src/main/java/com/modelacao/locacao_veiculo/service/UsuarioService.java
@@ -5,23 +5,35 @@ import com.modelacao.locacao_veiculo.repository.UsuarioRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 public class UsuarioService {
 
-    public UsuarioRepository usuarioRepository;
+    private UsuarioRepository usuarioRepository;
 
     public void registrar(Usuario usuario){
         System.out.println("############################### Printando o usuario: " + usuario);
         usuarioRepository.save(usuario);
     };
 
-    public void autenticar(){}
+    public boolean autenticar(String email, String senha){
+        Optional<Usuario> usuario = usuarioRepository.findByEmailAndSenha(email, senha);
+        if (usuario.isEmpty()) return false;
+        return true;
+    }
 
     public void locar(){}
 
-    public void eliminar(){}
+    public void eliminar(Long id){
+        usuarioRepository.deleteById(id);
+    }
 
-    public void vincularCnh(){}
+    public void vincularCnh(Long id){
+        Usuario novoUsuario = usuarioRepository.findById(id).orElseThrow();
+        novoUsuario.setCnh(novoUsuario.getCnh());
+        usuarioRepository.save(novoUsuario);
+    }
 
 }
